1b24157ad674f650a41064c050d8a0a88e5e49e5
Federico Barresi <federico.barresi@ams-osram.com>
2025-05-10 21:53:18 +0200

    added web layout

--- Signallama.Web/Hubs/ChatHub.cs
+++ Signallama.Web/Hubs/ChatHub.cs
@@ -6,6 +6,7 @@ namespace Signallama.Logic.Hubs;
 
 public class ChatHub : Hub<IWebClient>
 {
+    //see: https://learn.microsoft.com/en-us/aspnet/core/signalr/hubs?view=aspnetcore-9.0#inject-services-into-a-hub
     public async Task Send(string conversationId, string message, IChatClient client, ChatOptions options)
     {
         options.ConversationId = conversationId;
--- Signallama.Web/wwwroot/js/site.js
+++ Signallama.Web/wwwroot/js/site.js
@@ -1,20 +1,77 @@
 ﻿
 // create hub connection
-var connection = new signalR.HubConnectionBuilder().withUrl(signalRBaseAddress+"/hubs/clock").build();
+var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/chat").build();
 
 // perform stuff here before getting connected (e.g.: deactivate stuff...)
-//...
+updateConnectionStatus(false);
+
+document.getElementById('chatInput').addEventListener('keydown', function (event) {
+    const textValue = document.getElementById('chatInput').value;
+    if (textValue && event.key === 'Enter') {
+        sendMessage();
+    }
+});
 
 // create event handlers
-connection.on("ShowTime", function (message) {
-    document.getElementById("time").innerHTML = new Date(message).toLocaleString();
+connection.on("ShowReply", function (message) {
+    console.log(message);
+    appendMessage(false, `${message}`);
+});
+
+connection.onclose(() => {
+    updateConnectionStatus(false);
 });
 
 // start the connection
 connection.start().then(function () {
     //perform stuff here on connected
-    console.log("connection started!");
+    onConnected(connection);
 }).catch(function (err) {
     //on error
+    updateConnectionStatus(false);
     return console.error(err.toString());
 });
+
+function onConnected(connection) {
+    console.log(`connection started: ${connection.state}`);
+    updateConnectionStatus(true);
+}
+
+function updateConnectionStatus(isConnected) {
+    const statusElement = document.getElementById('connectionState');
+    if (isConnected) {
+        statusElement.innerHTML = 'Connected';
+        statusElement.classList.remove('text-bg-danger');
+        statusElement.classList.add('text-bg-success');
+    } else {
+        statusElement.innerHTML = 'Disconnected';
+        statusElement.classList.remove('text-bg-success');
+        statusElement.classList.add('text-bg-danger');
+    }
+}
+
+function sendMessage() {
+    const message = document.getElementById('chatInput').value;
+    if (message) {
+        appendMessage(true, message);
+        document.getElementById('chatInput').value = '';
+        connection.send("Send", "", message);
+    }
+}
+
+function appendMessage(isSender, message) {
+    const chatMessages = document.getElementById('chatMessages');
+    const messageElement = createMessageElement(message, isSender, null)
+    chatMessages.appendChild(messageElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+function createMessageElement(message, isSender, id) {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message', isSender ? 'sent' : 'received');
+    messageElement.innerText = message;
+    if (id) {
+        messageElement.id = id;
+    }
+    return messageElement;
+}
