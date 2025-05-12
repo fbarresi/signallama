5a2f8b46e3b1767c60cf5796fb997fa89ee6163e
Federico Barresi <federico.barresi@ams-osram.com>
2025-05-12 14:47:04 +0200

    changed from stream to simple model response

--- Signallama.Web/Hubs/ChatHub.cs
+++ Signallama.Web/Hubs/ChatHub.cs
@@ -10,13 +10,13 @@ public class ChatHub : Hub<IWebClient>
     //see: https://learn.microsoft.com/en-us/aspnet/core/signalr/hubs?view=aspnetcore-9.0#inject-services-into-a-hub
     public async Task Send(string conversationId, string message, IChatClient client, ChatOptions options)
     {
-        options.ConversationId = conversationId;
-        await foreach (var update in client.GetStreamingResponseAsync(message, options))
-        {
-            var regex = new Regex(@"\<think\>[\s\S]*\<\/think\>");
-            var reply = regex.Replace(update.Text, string.Empty).Trim();
-            await Clients.Caller.ShowReply(reply);
-        }
+        var opt = options.Clone();
+        opt.ConversationId = conversationId;
+
+        var response = await client.GetResponseAsync(message, options);
+        var regex = new Regex(@"\<think\>[\s\S]*\<\/think\>");
+        var reply = regex.Replace(response.Text, string.Empty).Trim();
+        await Clients.Caller.ShowReply(reply);
         
     }
 }
